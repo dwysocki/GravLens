a059b3a1bdb17dce49cc8205876cee9cb647f32d
Daniel Wysocki <dwysocki@oswego.edu>
2016-05-09 20:23:07 -0400

    Testing out potentials

--- src/main.py
+++ src/main.py
@@ -1,12 +1,34 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from scipy.constants import parsec, kilo, mega
+from astropy.constants import M_sun
+
 
 import lens
 import potential
 
 
 def main():
+    R = 2.2
+    D = 54
+    M = 1.2e15
+
+    r = np.linspace(0, R, 1000)
+    a = 1
+
+    Φ1 = potential.dehnen3D(r, γ=1, M=M, a=a)
+    Φ1_5 = potential.dehnen3D(r, γ=1.5, M=M, a=a)
+    Φ2_5 = potential.dehnen3D(r, γ=2.5, M=M, a=a)
+
+    plt.plot(r, -Φ1)
+    plt.plot(r, -Φ1_5)
+    plt.plot(r, -Φ2_5)
+    plt.loglog()
+    plt.show()
+
+    return
+
     dₒₗ = dₗₛ = ω = 1
     Φ2D = potential.zero2D
 
--- src/potential.py
+++ src/potential.py
@@ -2,23 +2,36 @@
 Gravitational potential equations.
 """
 import numpy as np
+from scipy.constants import G
+from scipy.integrate import quad
 
 
 def flatten(potential, R, D, θ, *args, **kwargs):
-    def r(s, θ):
-        return np.sqrt(s**2 + (D*np.tan(θ))**2)
+    Φ = np.empty_like(θ)
+    d = D*np.tan(θ)
+    s_max = np.sqrt(R**2 - d**2)
 
-    s = np.linspace(-R, R, 1000)
-    return potential(r(s, θ), *args, **kwargs)
+    def r(s, d):
+        return np.sqrt(s**2 + d**2)
+
+    for i in range(len(d)):
+        def potential_(s):
+            return potential(r(s, d[i]), *args, **kwargs)
+
+        Φ[i] = 2*quad(potential_, 0, s_max[i])
+
+    return Φ
 
 
 
 def dehnen3D(r, *, γ, M, a):
-    power = 2 - γ
-    const = -G*M / (a * power)
-    depend = 1 - (r / (r+a))**power
+    power  = 2 - γ
+    const  = -G*M / (a * power)
+    rest = 1 - (r / (r+a))**power
 
-    return const * depend
+    return const * rest
+
+
 
 
 
